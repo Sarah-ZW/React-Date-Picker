a0c99250f168ba4caffbef3e45237e6113df120e
Sarah-ZW <earthclaninfo@gmail.com>
2024-07-31 00:19:57 -0400

    new deployment file

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-# Exit on any error
-set -e
+echo "Switching to branch master"
+git checkout master
 
-# Ensure we're in the project directory
-cd /mnt/c/Users/bighe/Documents/GitHub/React-Date-Picker
-
-# Switch to main branch
-echo "Switching to branch main"
-git checkout main
-
-# Pull latest changes
-echo "Pulling latest changes"
-git pull origin main
-
-# Build the application
 echo "Building app"
 npm run build
 
-# Deploy the files to the server
 echo "Deploying files to server"
-rsync -avz --delete-after build/ earthclaninfo@triviabydate.sarahzw.com:/var/www/triviabydate
-
+rsync -avP build/ earthclaninfo@triviabydate.sarahzw.com:/var/www/triviabydate.sarahzw.com
 echo "Deployment complete"
